2398306d97337b2743cadf1dcb632146817ed420
unknown <omarmenjivar083@gmail.com>
2023-09-20 20:52:07 -0600

    Mi primer commit

---
+++ src/main/java/com/omar/dev/apirestpractica/ApiRestPracticaApplication.java
@@ -0,0 +1,13 @@
+package com.omar.dev.apirestpractica;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiRestPracticaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiRestPracticaApplication.class, args);
+	}
+
+}
